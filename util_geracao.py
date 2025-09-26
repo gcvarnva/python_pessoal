9193f656dae8831343fd2fb362b3e27e0912134f
gcvarnva <gcvarnva@gmail.com>
2025-09-26 17:06:01 -0300

    Arquivo com funções, constantes e etc., que são úteis para geração dos arquivos da pcat

---
+++ util_geracao.py
@@ -0,0 +1,30 @@
+# Dicionário: {'código IBGE': 'tamanho da IE'}
+ie_tamanhos_por_municipio = {
+    '12': '13',  # AC
+    '27': '9',   # AL
+    '13': '9',   # AM
+    '16': '9',   # AP
+    '29': '9',   # BA (na verdade pode ser 8 ou 9, mas colocamos 9 como regra geral)
+    '23': '9',   # CE
+    '53': '13',  # DF
+    '32': '9',   # ES
+    '52': '9',   # GO
+    '21': '9',   # MA
+    '31': '13',  # MG
+    '50': '9',   # MS
+    '51': '11',  # MT
+    '15': '9',   # PA
+    '25': '9',   # PB
+    '26': '9',   # PE (há casos com 14 dígitos, mas 9 é o padrão principal)
+    '22': '9',   # PI
+    '41': '10',  # PR
+    '33': '8',   # RJ
+    '24': '9',   # RN
+    '43': '10',  # RS
+    '11': '9',   # RO
+    '14': '9',   # RR
+    '42': '9',   # SC
+    '28': '9',   # SE
+    '35': '12',  # SP
+    '17': '9'    # TO
+}
